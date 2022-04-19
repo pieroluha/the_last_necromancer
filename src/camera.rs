d5f398b4802c4535e34aa003d6088d884195a6e4
pieroluha <pieroluha@protonmail.com>
2022-04-19 14:39:29 +0800

    Added camera scaling

--- src/camera.rs
+++ src/camera.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+use bevy::render::camera::ScalingMode;
 
 #[derive(Component)]
 pub struct MainCamera;
@@ -11,13 +12,22 @@ fn add_2d_camera(mut commands: Commands) {
 }
 
 fn add_ui_camera(mut commands: Commands) {
-    commands.spawn_bundle(UiCameraBundle::default());
+    commands
+        .spawn_bundle(UiCameraBundle::default())
+        .insert(Name::new("UiCamera"));
+}
+
+fn edit_camera_scaling(mut query_camera: Query<&mut OrthographicProjection, With<MainCamera>>) {
+    let mut camera = query_camera.single_mut();
+    camera.scaling_mode = ScalingMode::FixedVertical;
+    camera.scale = 200.0;
 }
 
 pub struct MyCameraPlugin;
 impl Plugin for MyCameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(SystemSet::on_enter(AssetLoad).with_system(add_2d_camera))
-            .add_system_set(SystemSet::on_enter(AssetLoad).with_system(add_ui_camera));
+            .add_system_set(SystemSet::on_enter(AssetLoad).with_system(add_ui_camera))
+            .add_system_set(SystemSet::on_enter(Playing).with_system(edit_camera_scaling));
     }
 }
